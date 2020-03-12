d20627625a0f2270494ac22af99b9a0933d76211
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-03-12 20:48:30 +0700

    unisntall node-crone

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const cron = require('node-cron')
 const port =  process.env.PORT || 3000;
 const warp = require('./buffwarp');
-cron.schedule('* * * * *', () => {
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('* * * * *', function() {
     warp.show();
     warp.init();
-});
+}, null, true, 'America/Los_Angeles');
+job.start();
+
 const app =  express();
 app.get('/', (req, res) => {
     res.render('index.html');
