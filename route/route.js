283290993d64307631edb03255cf54abed0a175d
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-03-16 20:43:03 +0700

    version 0

--- route/route.js
+++ route/route.js
@@ -6,20 +6,20 @@ module.exports = function (app) {
         res.render('indexGet')
     })
     app.post('/', async function (req, res) {
-        // var id = JSON.parse(JSON.stringify(req.body)).id;
-        // const existed = await model.insertID(id);
-        // if (existed == true) {
-        //     res.render('indexPost',
-        //         {
-        //             title: "Đã tiếp nhận thông tin. Chờ đợi là hạnh phúc"
-        //         })
-        // }
-        // else {
-        //     res.render('indexPost',
-        //         {
-        //             title: "ID đã tồn tại nha"
-        //         })
-        // }
+        var id = JSON.parse(JSON.stringify(req.body)).id;
+        const existed = await model.insertID(id);
+        if (existed == true) {
+            res.render('indexPost',
+                {
+                    title: "Đã tiếp nhận thông tin. Chờ đợi là hạnh phúc"
+                })
+        }
+        else {
+            res.render('indexPost',
+                {
+                    title: "ID đã tồn tại nha"
+                })
+        }
     })
     app.post('/delete', async function (req, res) {
         var id = JSON.parse(JSON.stringify(req.body)).id;
